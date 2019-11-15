ffdd21e3f89ccd2a3dafac172ccd039784be2cce
maciekziol <maciekziol@wp.pl>
2019-11-15 18:55:37 +0100

    FIXed chb controller

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module Fajranthub
   class Application < Rails::Application
+    config.web_console.whitelisted_ips = '66.249.83.207'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
